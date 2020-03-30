f280c68f899edc88a844a3be570c6ce6ce1adde9
clauries <55369403+clauries@users.noreply.github.com>
2020-03-30 16:14:09 -0700

    Added npm packages for Router & Axios; created pages & navbar; added front-end routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from './pages/Search.js';
+import Saved from './pages/Saved.js';
+import NoMatch from './pages/NoMatch.js';
+import Nav from "./components/Nav";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={"/"}>
+            <Search />
+          </Route>
+
+          <Route exact path={"/search"}>
+            <Search />
+          </Route>
+
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+
+          <Route>
+            <NoMatch />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoMatch() {
+  return (
+      <div>
+        <h1>404 error, page not found</h1>
+      </div>
+    
+  );
+}
+
+export default NoMatch;
