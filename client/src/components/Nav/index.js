7bef2f0f22609b5af538d28f3bab421fa762583c
clauries <55369403+clauries@users.noreply.github.com>
2020-04-01 16:09:54 -0700

    Added db & files connected to db

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  button: {
+    color: "white",
+  }
+}));
 
 function Nav() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Books
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <MenuBookIcon /> 
+        <Typography variant="h6" className={classes.title}>
+             BooKey
           </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
+          <Button className={classes.button} href="/">Search</Button>
+          <Button className={classes.button} href="/saved">Saved</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
 
-    );
+  );
 }
 
 export default Nav;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Button, TextField, Box } from '@material-ui/core';
+import { borders } from '@material-ui/system';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1, 
+  },
+  title: {
+    flexGrow: 1,
+  },
+  button: {
+    color: "white",
+  },
+
+}));
 
 function Search() {
+  const classes = useStyles();
+
   return (
-      <div>
-        <h1>Search Page</h1>
-      </div>
-    
+
+    <div className={classes.root}>
+      <Box
+        border={1}
+        borderRadius={16}
+        borderColor="grey.500"
+        padding="16px"
+        margin="2% 10%" >
+        <Typography variant="h2" className={classes.title}>
+          Search Google Books
+      </Typography>
+        <Typography variant="h5" className={classes.title}>
+          What do you want to search for?
+      </Typography>
+        <TextField />
+      </Box>
+      <Box
+        border={1}
+        borderRadius={16}
+        borderColor="grey.500"
+        padding="16px"
+        margin="2% 10%">
+        <Typography variant="h3">Results</Typography>
+        
+      </Box>
+    </div>
+
   );
 }
 
